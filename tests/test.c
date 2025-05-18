b8d5801b42db03f35cb81875106e56f731e86b85
Smothyy <winix399@gmail.com>
2025-05-18 21:54:52 +0200

    first test with ncurses

--- tests/test.c
+++ tests/test.c
@@ -1,6 +1,26 @@
 #include <ncurses.h>
-#include <panel.h>
 
 int main() {
+    int ch;
+    
+    initscr();
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
 
+    printw("Type any character, or 'q' to quit the program!\n");
+
+    ch = getch();
+
+    if (ch != 'q') {
+        printw("You typed %c\n", ch);
+        getch();
+    }
+    else printw("Quite...");
+    endwin();
+
+    return 0;
 }
+
+
+// gcc tests/test.c -lncurses -lmenu
