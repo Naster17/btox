2c14977c39baf357e71874417c2f920b0f30e0d3
Naster17 <naster17@hacker.is>
2025-05-18 20:24:13 +0200

    Hot Fix

--- src/core/argpars.c
+++ src/core/argpars.c
@@ -7,6 +7,15 @@
 // short version for if
 #define sc strcmp
 
+struct Args
+{
+    char flag[32];                       // main flag
+    char s_flag[16];                     // if hase short version arg
+    char desc[128];                      // description
+    int req_args;                        // number of required arguments
+    void (*func)(int pos, char *argv[]); // function
+};
+
 bool notarg(const char *s)
 {
     if (strstr("-", s) == NULL)
@@ -15,25 +24,33 @@ bool notarg(const char *s)
     return false;
 }
 
-void help()
+void help(int pos, char **argv)
 {
     printf("Args:\n"
            "  --help\tTo see this message\n"
            "  --save\tUse save file\n");
 }
 
+struct Args args[2] = {{"--help", "-h", "Help menu", 0, help}};
+
 void argsparse(int argc, char *argv[])
 {
+
     for (int i = 0; i < argc; i++)
     {
-        // printf("%s\n", argv[i]);
-        if (!sc(argv[i], "--help") || !sc(argv[i], "-h"))
+        for (int j = 0; j < sizeof(args) / sizeof(args[0]); j++)
         {
-            help();
+            printf("%s %s\n", args[j].flag, args[j].s_flag);
+            args[j].func(0, argv);
         }
-        else if (!sc(argv[i], "--save"))
-        {
-            printf("Save\n");
-        }
+        // // printf("%s\n", argv[i]);
+        // if (!sc(argv[i], "--help") || !sc(argv[i], "-h"))
+        // {
+        //     help();
+        // }
+        // else if (!sc(argv[i], "--save"))
+        // {
+        //     printf("Save\n");
+        // }
     }
 }
