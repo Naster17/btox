a715dc49a2cdfd35780464041b9e2da8def8a504
Smothyy <winix399@gmail.com>
2025-05-20 22:27:55 +0200

    add inputFunc.c, and using user input ncurses..

--- tests/test.c
+++ tests/test.c
@@ -7,6 +7,7 @@ int main()
     int row, col;
 
     initscr();
+<<<<<<< HEAD
     while (1)
     {
         clear();
@@ -19,6 +20,25 @@ int main()
         printw("Try resizing your window(if possible) and then run this program again");
         refresh();
     }
+=======
+    // while (1)
+    // {
+        // clear();
+        initscr();
+        // getmaxex() is a func to get max horizontal and vertical size of window...
+        getmaxyx(stdscr, row, col);
+        // equal to move(row / 2, (col - strlen(msg)) / 2); printw("%s", msg); in one func.
+        mvprintw(row / 2, (col - strlen(msg)) / 2, "%s", msg);
+        // 
+        // All this func take y, x cordinate NOT x, y. It's a common mistake by beginners.
+        // 
+        mvprintw(row - 2, 0, "This screen has %d rows and %d columns\n", row, col);
+        printw("Try resizing your window(if possible) and then run this program again");
+
+        refresh();
+    // }
+
+>>>>>>> c033e74 (add inputFunc.c, and using user input ncurses..)
     getch();
     endwin();
 
